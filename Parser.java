e74266be3690ece192ecba29db3622f352951cdd
Daniel <daniel.dietzler@web.de>
2021-01-23 14:26:52 +0100

    Massive refactoring.

--- Parser.java
+++ Parser.java
@@ -2,78 +2,66 @@ public class Parser {
     public static BinaryTree<String> parseString(String string) {
         string = string.trim().replaceAll(",", ".");
         BinaryTree<String> tree = new BinaryTree<>("");
-        char[] arr = string.toCharArray();
-        int k = 0;
+        char[] stringArray = string.toCharArray();
+        int operatorPosition = 0;
 
-        for (int i = 0; i < string.length(); i++) {
-            char c = arr[i];
-            BinaryTree<String> left;
-            BinaryTree<String> right;
+        for (int index = 0; index < string.length(); index++) {
+            char currentChar = stringArray[index];
 
-            switch (c) {
-                case '(':
-                    int pos = string.indexOf(')', i);
+            switch (currentChar) {
+                case '(' -> {
+                    int pos = string.indexOf(')', index);
                     if (pos == string.length() - 1)
-                        tree.setRightTree(parseString(string.substring(i + 1, pos)));
-                    i = pos;
-                    break;
-                case '*':
-                    if (arr[i - 1] == ')')
-                        left = parseString(string.substring(1, i - 1));
-                    else
-                        left = new BinaryTree<>(string.substring(0, i));
-                    tree = new BinaryTree<>("*");
-                    tree.setLeftTree(left);
-                    k = i;
-                    break;
-                case '/':
-                    if (arr[i - 1] == ')')
-                        left = parseString(string.substring(1, i - 1));
-                    else
-                        left = new BinaryTree<>(string.substring(0, i));
-                    tree = new BinaryTree<>("/");
-                    tree.setLeftTree(left);
-                    k = i;
-                    break;
-                case '+':
-                    if (!tree.isEmpty() && (tree.getContent().equals("*") || tree.getContent().equals("/"))) {
-                        if (arr[k + 1] == '(')
-                            right = parseString(string.substring(k + 2, i - 1));
-                        else
-                            right = new BinaryTree<>(string.substring(k + 1, i));
-                        tree.setRightTree(right);
-                        tree = new BinaryTree<>("+", tree, parseString(string.substring(i + 1)));
-                        return tree;
-                    }
-                    if (arr[i - 1] == ')')
-                        left = parseString(string.substring(1, i - 1));
-                    else
-                        left = new BinaryTree<>(string.substring(0, i));
-                    tree = new BinaryTree<>("+", left, parseString(string.substring(i + 1)));
-                    return tree;
-                case '-':
-                    if (!tree.isEmpty() && (tree.getContent().equals("*") || tree.getContent().equals("/"))) {
-                        if (arr[k + 1] == '(')
-                            right = parseString(string.substring(k + 2, i - 1));
-                        else
-                            right = new BinaryTree<>(string.substring(k + 1, i));
-                        tree.setRightTree(right);
-                        tree = new BinaryTree<>("-", tree, parseString(string.substring(i + 1)));
-                        return tree;
-                    }
-                    if (arr[i - 1] == ')')
-                        left = parseString(string.substring(1, i - 1));
-                    else
-                        left = new BinaryTree<>(string.substring(0, i));
-                    tree = new BinaryTree<>("-", left, parseString(string.substring(i + 1)));
-                    return tree;
-                default:
-                    if (k > 0 && i == string.length() - 1) {
-                        right = new BinaryTree<>(string.substring(k + 1));
-                        tree.setRightTree(right);
-                    } else if (i == string.length() - 1)
+                        tree.setRightTree(parseString(string.substring(index + 1, pos)));
+                    index = pos;
+                }
+
+                case '*', '/' -> {
+                    tree = calculateHigherLevelOperator(string, stringArray, index, tree);
+                    operatorPosition = index;
+                }
+
+                case '+', '-' -> {
+                    return calculateLowLevelOperator(string, stringArray, index, operatorPosition, tree);
+                }
+
+                default -> {
+                    if (operatorPosition > 0 && index == string.length() - 1) {
+                        tree.setRightTree(new BinaryTree<>(string.substring(operatorPosition + 1)));
+                    } else if (index == string.length() - 1)
                         tree = new BinaryTree<>(string);
+                }
             }
+        }
+        return tree;
+    }
+
+    public static BinaryTree<String> calculateLowLevelOperator(String string, char[] stringArray, int index, int operatorPosition, BinaryTree<String> tree) {
+        String currOperator = String.valueOf(stringArray[index]);
+
+        if (!tree.isEmpty() && (tree.getContent().equals("*") || tree.getContent().equals("/"))) {
+            if (stringArray[operatorPosition + 1] == '(')
+                tree.setRightTree(parseString(string.substring(operatorPosition + 2, index - 1)));
+            else
+                tree.setRightTree(new BinaryTree<>(string.substring(operatorPosition + 1, index)));
+            tree = new BinaryTree<>(currOperator, tree, parseString(string.substring(index + 1)));
+            return tree;
+        }
+        if (stringArray[index - 1] == ')')
+            tree = new BinaryTree<>(currOperator, parseString(string.substring(1, index - 1)), parseString(string.substring(index + 1)));
+        else
+            tree = new BinaryTree<>(currOperator, parseString(string.substring(0, index)), parseString(string.substring(index + 1)));
+        return tree;
+    }
+
+    public static BinaryTree<String> calculateHigherLevelOperator(String string, char[] stringArray, int index, BinaryTree<String> tree) {
+        String currOperator = String.valueOf(stringArray[index]);
+
+        tree = new BinaryTree<>(currOperator);
+        if (stringArray[index - 1] == ')') {
+            tree.setLeftTree(parseString(string.substring(1, index - 1)));
+        } else {
+            tree.setLeftTree(new BinaryTree<>(string.substring(0, index)));
         }
         return tree;
     }
